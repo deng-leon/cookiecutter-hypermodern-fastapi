419de72f512116cc18023d6f5cb137a29b51a332
deng-leon <20753655+deng-leon@users.noreply.github.com>
2022-07-09 07:59:39 +0200

    Update __main__.py

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -1,12 +1,18 @@
-"""Command-line interface."""
-import click
+"""Hello {{cookiecutter.friendly_name}}"""
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-@click.command()
-@click.version_option()
-def main() -> None:
-    """{{cookiecutter.friendly_name}}."""
+@app.get("/")
+def read_root():
+    return {"Hello": "{{cookiecutter.friendly_name}}"}
+
+
+def main():
+    uvicorn.run("{{cookiecutter.project_name}}.__main__:app", host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
-    main(prog_name="{{cookiecutter.project_name}}")  # pragma: no cover
+    main()
